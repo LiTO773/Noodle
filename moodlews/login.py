1619d4aa2329f736299e0aca796abd4bb1b00be2
LiTO773 <1180965@isep.ipp.pt>
2020-09-26 18:38:51 +0100

    fix(moodlews): Requests are now properly done when the URL doesn't end with /

--- moodlews/login.py
+++ moodlews/login.py
@@ -6,7 +6,7 @@ from errors.error_codes import UNABLE_TO_LOGIN
 def login(config: dict) -> str:
     """ This function is used to login to moodle. It returns the token if it was successful, otherwise returns
     UNABLE_TO_LOGIN """
-    url = config['host'] + ('/' if config['host'][-1] == '/' else '') + 'login/token.php'
+    url = config['host'] + ('/' if config['host'][-1] != '/' else '') + 'login/token.php'
     params = {'username': config['username'], 'password': config['password'], 'service': 'moodle_mobile_app'}
 
     response = requests.post(url, params=params)
--- moodlews/moodle_request.py
+++ moodlews/moodle_request.py
@@ -6,7 +6,7 @@ from model.infos import Infos
 
 def _moodle_request(infos: Infos, wsfunction: str, extras: dict = {}) -> dict:
     """ This is an helper function that streamlines the requests made to Moodle's Web Services """
-    url = infos.host + ('/' if infos.host[-1] == '/' else '') + 'webservice/rest/server.php'
+    url = infos.host + ('/' if infos.host[-1] != '/' else '') + 'webservice/rest/server.php'
     params = {'wstoken': infos.token, 'wsfunction': wsfunction, 'moodlewsrestformat': 'json'}
     params.update(extras)
 
