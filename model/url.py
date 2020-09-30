92f5589f5b35a5c039f29e516bd875beb95f2a0b
LiTO773 <1180965@isep.ipp.pt>
2020-09-30 18:21:47 +0100

    fix(domain): URL now has the proper params

--- model/url.py
+++ model/url.py
@@ -3,12 +3,23 @@ from model.ConvertToDict import ConvertToDict
 
 class URL(ConvertToDict):
     """ This class stores the information of a Moodle folder """
-    def __init__(self, body: dict, download=True):
-        self.__filename = body['filename']
-        self.__filesize = body['filesize']
-        self.__fileurl = body['fileurl']
+    @staticmethod
+    def create_from_db(info: tuple):
+        url = URL(info[2], info[3], info[4], info[5])
+        url.__set_remaining_params(info[6], info[7])
+        return url
+
+    def __init__(self, filename: str, fileurl: str, timemodified: int, download: bool = False):
+        self.__filename = filename
+        self.__fileurl = fileurl
+        self.__timemodified = timemodified
         self.__download = download
         self.__downloaded = False
+
+    def __set_remaining_params(self, downloaded, current_hash):
+        """ Used to add parameters already available in the DB """
+        self.__downloaded = downloaded
+        self.__current_hash = current_hash
 
     def to_dict(self):
         return {
@@ -22,3 +33,15 @@ class URL(ConvertToDict):
 
     def __hash__(self):
         return hash((self.__filename, self.__filesize, self.__fileurl))
+
+    def get_name(self):
+        return self.__filename
+
+    def get_url(self):
+        return self.__fileurl
+
+    def get_time_modified(self):
+        return self.__timemodified
+
+    def get_download(self):
+        return self.__download
